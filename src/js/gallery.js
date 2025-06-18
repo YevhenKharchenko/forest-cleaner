3fd9085bcb7778be18f9d53f32832cbed9399356
YevhenKharchenko <evgenvalerych@gmail.com>
2025-06-18 10:54:10 +0300

    Initial commit

---
+++ src/js/gallery.js
@@ -0,0 +1,32 @@
+import Swiper from 'swiper';
+import 'swiper/css/bundle';
+
+let gallerySwiper;
+
+gallerySwiper = new Swiper('.gallery-swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  grabCursor: true,
+  slidesPerView: 1,
+  initialSlide: 0,
+  spaceBetween: 20,
+  grabCursor: true,
+  allowTouchMove: true,
+  speed: 500,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    1440: {
+      slidesPerView: 4,
+      grabCursor: false,
+      allowTouchMove: false,
+    },
+  },
+  on: {
+    init: () => {
+      document.querySelector('.gallery-swiper-container').classList.add('show');
+    },
+  },
+});
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './js/header.js';
+import './js/gallery.js';
+import './js/reviews.js';
+import './js/banner.js';
